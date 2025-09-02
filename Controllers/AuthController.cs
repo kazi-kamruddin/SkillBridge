16082fac547aa7dc1178c7ec6fae01df37945998
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-09-02 19:12:06 +0600

    added pages with routing (#1)

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillSwap.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
